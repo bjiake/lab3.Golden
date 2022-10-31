15bd4d9879f726d286d65781e8fa4eb3e525509c
bjiake <suyd@list.ru>
2022-10-31 23:11:40 +0700

    Добавьте файлы проекта.

---
+++ lab3/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace lab3 {
+    partial class Form1 {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && ( components != null )) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent() {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            System.Windows.Forms.DataVisualization.Charting.Title title2 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbLeftBorder = new System.Windows.Forms.TextBox();
+            this.tbRightBorder = new System.Windows.Forms.TextBox();
+            this.tbInput = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.AxisY.Maximum = 6D;
+            chartArea1.AxisY.MaximumAutoSize = 100F;
+            chartArea1.AxisY.Minimum = -4D;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Location = new System.Drawing.Point(23, 12);
+            this.chart1.Name = "chart1";
+            series1.BorderWidth = 6;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Color = System.Drawing.Color.Red;
+            series1.LabelBorderWidth = 3;
+            series1.Name = "Series1";
+            series2.BorderWidth = 3;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series2.LabelBackColor = System.Drawing.Color.Red;
+            series2.LabelBorderColor = System.Drawing.Color.Red;
+            series2.LabelBorderWidth = 6;
+            series2.LabelForeColor = System.Drawing.Color.Red;
+            series2.Name = "Series2";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(1078, 353);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
+            title1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            title1.Name = "Title1";
+            title1.Text = "Время";
+            title2.Docking = System.Windows.Forms.DataVisualization.Charting.Docking.Left;
+            title2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            title2.IsDockedInsideChartArea = false;
+            title2.Name = "Title2";
+            title2.Text = "Деньги";
+            this.chart1.Titles.Add(title1);
+            this.chart1.Titles.Add(title2);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(107, 392);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(231, 25);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Формула для расчета";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(107, 460);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(150, 25);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Крайний срок";
+            // 
+            // tbLeftBorder
+            // 
+            this.tbLeftBorder.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbLeftBorder.Location = new System.Drawing.Point(285, 454);
+            this.tbLeftBorder.Name = "tbLeftBorder";
+            this.tbLeftBorder.Size = new System.Drawing.Size(97, 31);
+            this.tbLeftBorder.TabIndex = 7;
+            // 
+            // tbRightBorder
+            // 
+            this.tbRightBorder.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbRightBorder.Location = new System.Drawing.Point(388, 454);
+            this.tbRightBorder.Name = "tbRightBorder";
+            this.tbRightBorder.Size = new System.Drawing.Size(97, 31);
+            this.tbRightBorder.TabIndex = 8;
+            // 
+            // tbInput
+            // 
+            this.tbInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbInput.Location = new System.Drawing.Point(344, 392);
+            this.tbInput.Name = "tbInput";
+            this.tbInput.Size = new System.Drawing.Size(357, 31);
+            this.tbInput.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(112, 505);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(210, 31);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Вычислить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1124, 581);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tbInput);
+            this.Controls.Add(this.tbRightBorder);
+            this.Controls.Add(this.tbLeftBorder);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbLeftBorder;
+        private System.Windows.Forms.TextBox tbRightBorder;
+        private System.Windows.Forms.TextBox tbInput;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ lab3/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using org.mariuszgromada.math.mxparser;
+using org.mariuszgromada.math.mxparser.mathcollection;
+using org.mariuszgromada.math.mxparser.parsertokens;
+using org.mariuszgromada.math.mxparser.syntaxchecker;
+
+namespace lab3 {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e) {
+            if (tbInput.Text == string.Empty || tbRightBorder.Text == string.Empty || tbLeftBorder.Text == string.Empty) {
+                MessageBox.Show("Пожалуйста, введите данные");
+                return;
+            }
+            chart1.ChartAreas[0].AxisX.Minimum = Convert.ToInt32(tbLeftBorder.Text);
+            chart1.ChartAreas[0].AxisX.Maximum = Convert.ToInt32(tbRightBorder.Text);
+
+            DrawChart();
+            DrawGolden();
+        }
+
+        #region Ньютони
+        private double e = 0.01;
+        private double gLeftBorder;
+        private double gRightBorder;
+        private double k2;
+        private double k1;
+        private double x1;
+        private double x2;
+        private double F1;
+        private double F2;
+        
+        private double derivateRigthBorder;
+        private double derivateLeftBorder;
+        private double GoldenNewton() {
+            k2 = 0; k1 = 0; x1 = 0; x2 = 0; F1 = 0; F2 = 0; gX = 0; gY = 0;
+            gLeftBorder = Convert.ToDouble(tbLeftBorder.Text);
+            gRightBorder = Convert.ToDouble(tbRightBorder.Text);
+
+            k2 = ( Math.Sqrt(5) - 1 ) / 2;
+            k1 = 1 - k2;
+
+            x1 = gLeftBorder + k1 * ( gRightBorder - gLeftBorder );
+            x2 = gLeftBorder + k2 * ( gRightBorder - gLeftBorder );
+
+            F1 = parseMath(x1);
+            F2 = parseMath(x2);
+
+            while(true) {
+                derivateLeftBorder = parseDerivate(gLeftBorder);
+                derivateRigthBorder = parseDerivate(gRightBorder);
+
+                if(derivateRigthBorder <= -200) {
+                    flag = true;
+                    return derivateRigthBorder;
+                }
+                for (double i = gLeftBorder; i < gLeftBorder + 3; i += 0.1) {
+                    if(parseDerivate(i) <= -200) {
+                        flag = true;
+                        return i;
+                    }
+                }
+
+                if(Math.Abs(gRightBorder - gLeftBorder) < e) {
+                    return ( gLeftBorder + gRightBorder ) / 2;
+                }
+                if(F1 < F2) {
+                    gRightBorder = x2;
+                    x2 = x1;
+                    F2 = F1;
+
+                    x1 = gLeftBorder + k1 * ( gRightBorder - gLeftBorder );
+                    F1 = parseMath(x1);
+                } else {
+                    gLeftBorder = x1;
+                    x1 = x2;
+                    F1 = F2;
+
+                    x2 = gLeftBorder + k2 * ( gRightBorder - gLeftBorder );
+                    F2 = parseMath(x2);
+                }
+            }
+        }
+        #endregion
+
+        #region строим ньютона
+        private double gX;
+        private double gY;
+        private bool flag = false;
+        private void DrawGolden() {
+            GoldenNewton();
+            this.chart1.Series[1].Points.Clear();
+            gX = GoldenNewton();
+            gY = parseMath(gX);
+            chart1.ChartAreas[0].AxisY.Minimum = gY;
+
+            this.chart1.Series[1].Points.AddXY(gX, gY);
+            if (!flag) {
+                MessageBox.Show($"Минимальная точка {gX}, {gY}");
+            } else {
+                MessageBox.Show($"Точка стремится к бесконечности в {gX}");
+            }
+        }
+        #endregion
+
+        #region построение графика
+
+        private double leftBorder;
+        private double rightBorder;
+        private double step = 0.1;
+        private double x;
+        private double y = 0;
+
+        private void DrawChart() {
+            leftBorder = Convert.ToDouble(tbLeftBorder.Text);
+            rightBorder = Convert.ToDouble(tbRightBorder.Text);//Правая граница
+            x = leftBorder;
+
+            //Очистка графика
+            this.chart1.Series[0].Points.Clear();
+
+            while (x <= rightBorder) {
+                y = parseMath(x);
+                if (double.IsNaN(y)) {
+                    x++;
+                    continue;
+                }
+                this.chart1.Series[0].Points.AddXY(x, y);
+                x += step;
+            }
+        }
+        #endregion
+
+        #region парсинг
+        private double parseMath(double point) {
+            Argument x = new Argument("x");
+            x.setArgumentValue(point);
+            Expression expression = new Expression(tbInput.Text.ToString(), x);
+            return expression.calculate();
+        }
+        #endregion
+
+        #region Производная вычислять
+        private double parseDerivate(double point) {
+            Argument x = new Argument("x");
+            x.setArgumentValue(point);
+            string formula = tbInput.Text.ToString();
+            string derivateFormula = "der(" + formula + ",x)";
+            Expression expression = new Expression(derivateFormula, x);
+            return expression.calculate();
+        }
+
+        #endregion
+    }
+}
